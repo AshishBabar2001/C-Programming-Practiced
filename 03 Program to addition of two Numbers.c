b0b8ec48073a1053132d0e79483ce91bfd648587
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-21 00:35:39 +0530

    Add files via upload

---
+++ 03 Program to addition of two Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int n1,n2,Sum;
+    printf("\n Enter First Number : ");
+    scanf("%d",&n1);
+    printf("\n Enter Second Number : ");
+    scanf("%d",&n2);
+    Sum = n1 + n2;
+    printf("\n Summation Of %d + %d = %d",n1,n2,Sum);
+    getch();
+}
