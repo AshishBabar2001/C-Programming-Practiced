1d3aac11fd0569c6868267ceebfb26da2fff6bdb
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-25 02:09:36 +0530

    Add files via upload

---
+++ 08 Program To Multiplying Two Numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    int n1,n2,Mul;
+    printf("\n Enter A Number To Check It's Multiplication : ");
+    scanf("%d %d",&n1,&n2);
+    Mul = n1 * n2;
+    printf("\n Multiplication of Two Numbers %d * %d Is %d",n1,n2,Mul);
+    getch();
+}
