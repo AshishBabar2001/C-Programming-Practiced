27deac3ea986b9873b722025f04506089bf70fdb
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-25 02:09:28 +0530

    Add files via upload

---
+++ 07 Program To Sudstrating Two Numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    int n1,n2,Sub;
+    printf("\n Enter A Number To Check It's Substration : ");
+    scanf("%d %d",&n1,&n2);
+    Sub = n1 - n2;
+    printf("\n Substraction of Two Numbers %d - %d Is %d",n1,n2,Sub);
+    getch();
+}
