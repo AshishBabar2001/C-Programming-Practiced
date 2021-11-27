b80279e4832ae45f314cfd36d9faba84d134f435
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-28 00:51:36 +0530

    Add files via upload

---
+++ 12 Accept Three Numbers from user And Print Maximum of Them using Nested If Statement.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+main()
+{
+    int n1,n2,n3;
+
+    printf("\n Enter a Number : ");
+    scanf("%d %d %d",&n1,&n2,&n3);
+
+    if(n1 > n2)
+    {
+        if(n1 > n3)
+        {
+            printf("\n Max Is n1 : %d",n1);
+        }
+    }
+    else if(n2 > n1)
+    {
+        if(n2 > n3)
+        {
+            printf("\n Max Is n2 : %d",n2);
+        }
+    }
+    else if(n3 > n1)
+    {
+        if(n3 > n2)
+        {
+            printf("\n Max Is n3 : %d",n3);
+        }
+    }
+    else
+        {
+            printf("\n All Numbers Are Equal");
+        }
+    getch();
+}
