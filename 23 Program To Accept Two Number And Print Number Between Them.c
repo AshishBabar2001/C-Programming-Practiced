8223de41bcfea422bcb4925339d52c288604cb40
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-04 01:02:49 +0530

    Add files via upload

---
+++ 23 Program To Accept Two Number And Print Number Between Them.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+    int n1,n2;
+
+    printf("\n Enter Two Numbers : ");
+    scanf("%d %d",&n1,&n2);
+
+    if(n1 < n2)
+    {
+        while(n1 <= n2)
+     {
+         printf("\n %d",n1);
+         n1 = n1 + 1;
+     }
+    }
+    else
+    {
+        while(n1 >= n2)
+     {
+         printf("\n %d",n1);
+         n1 = n1 - 1;
+     }
+    }
+     getch();
+}
