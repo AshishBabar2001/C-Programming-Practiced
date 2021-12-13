6acc6923e530d41433d1c15c32f6cd7b9c711ea8
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-14 00:59:02 +0530

    Add files via upload

---
+++ 40 Asending And Desending Order.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+    int n1,n2;
+    printf("Enter Two Numbers : ");
+    scanf("%d %d",&n1,&n2);
+    if(n1 > n2)
+    {
+    do
+        {
+            printf("\n %d",n1);
+            n1 = n1 - 1;
+        }while(n1 >= n2);
+    }
+    else
+    {
+    do
+        {
+            printf("\n %d",n1);
+            n1 = n1 + 1;
+        }while(n1 <= n2);
+    }
+
+}
