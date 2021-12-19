8cf28e1c599e3bd2da03dfc48205acb75463ed79
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-20 01:57:18 +0530

    Add files via upload

---
+++ 60 Function Defining Program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+Fun()
+{
+    printf("\n\n I am In User Define Function");
+    printf("\nHello World");
+}
+
+main()
+{
+    Fun();
+    printf("\n \n I am In Main");
+    getch();
+}
