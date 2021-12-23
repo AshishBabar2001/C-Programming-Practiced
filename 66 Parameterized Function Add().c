61300fb974f0644efe46336b8f1f23e74a8cc2bf
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-24 01:44:23 +0530

    Add files via upload

---
+++ 66 Parameterized Function Add().c
@@ -0,0 +1,22 @@
+#include<Stdio.h>
+Add (int n1,int n2)
+{
+    int Sum;
+    Sum = n1 + n2;
+
+    printf("\n Summation Is %d",Sum);
+
+}
+Sub (int n1,int n2)
+{
+    int Sub;
+    Sub = n1 - n2;
+
+    printf("\n Summation Is %d",Sub);
+
+}
+main()
+{
+    Add(100,30);
+    Sub(100,30);
+}
