4c6c2186c748e7c201f2326ae459a997ab268b41
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-24 01:44:48 +0530

    Add files via upload

---
+++ 69 Array Element Program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main ()
+{
+    int Arr[3];
+    Arr[0] = 101;
+    Arr[1] = 102;
+    Arr[2] = 103;
+
+    printf("\n First Array Is %d",Arr[0]);
+    printf("\n Second Array Is %d",Arr[1]);
+    printf("\n Third Array Is %d",Arr[2]);
+    getch();
+}
