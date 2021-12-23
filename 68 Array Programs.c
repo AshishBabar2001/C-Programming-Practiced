75b8991f45701789be18babb985f46000ad219ed
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-24 01:44:40 +0530

    Add files via upload

---
+++ 68 Array Programs.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+    int Arr[5],i;
+
+    for(i=0;i<5;i++)
+    {
+        printf("\n Enter Array Element");
+        scanf("%d",Arr[i]);
+    }
+    printf("\n Elements Are : ");
+    printf("%d",Arr[i]);
+}
