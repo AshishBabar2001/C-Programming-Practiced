776347b0a0fc9351d9f1a0a1f866d954f7b9d49d
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-02-18 01:07:06 +0530

    Add files via upload

---
+++ 93 String Library Function Copy (strcpy).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    char str1[20] = "Pheonix";
+    char str2[20];
+
+    strcpy(str2,str1);
+
+    printf("\n String 1 Is : %s",str1);
+    printf("\n String 2 Is : %s",str2);
+}
